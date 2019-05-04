d2370dc2df98a78bfd405e2344d0d696e31c9e68
Razvan Rosu <razvan-rosu89@gmail.com>
2019-05-04 10:12:31 +0300

    Add spacing

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,15 @@ import SEO from "../components/seo";
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
+
     <Link to="/articles/">Go to Articles</Link>
     <Link to="/contact/">Go to Contact</Link>
     <Link to="/codes/">Go to Codes</Link>
