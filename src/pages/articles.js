a6f154b69e7268d309c4b6766c0f22bde30f1ee0
Razvan Rosu <razvan-rosu89@gmail.com>
2019-05-05 18:50:44 +0300

    Add graphql query for all blog posts and iterate articles

--- src/pages/articles.js
+++ src/pages/articles.js
@@ -4,13 +4,40 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const ArticlesPage = () => (
+const ArticlesPage = ({data}) => (
   <Layout>
     <SEO title="Page Articles" />
     <h1>Hi from the Articles page</h1>
     <p>Welcome to Articles</p>
+
+    <ul>
+      {data.allMarkdownRemark.edges.map(post => (
+        <li key={post.node.id}>
+          <a href={post.node.frontmatter.path}>
+            {post.node.frontmatter.title}
+          </a>
+        </li>
+      ))}
+    </ul>
+
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 );
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit: 10) {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default ArticlesPage;
