2c171ca58f25bf6c605c75213206bd7f2df39461
razvan-rosu <razvan-rosu@outlook.com>
2020-11-15 01:25:29 +0200

    refactor: set articles page as homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,66 @@
 import React from "react";
+import { Link, graphql } from "gatsby";
+import { FiArrowUp } from 'react-icons/fi';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-
-    <article className="prose mx-auto">
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-    </article>
+    <h1 className="text-4xl font-extrabold text-midnight">Articles</h1>
+    <h2 className="text-2xl font-bold text-midnight">Here's what I've been up to:</h2>
+    <ul>
+      {data.allMarkdownRemark.edges.map(post => (
+        <li key={post.node.id} className="my-5">
+          <div className="w-full border border-concrete lg:border lg:border-gray-400 bg-white rounded-b lg:rounded p-4 flex flex-col justify-between leading-normal">
+            <h3 className="text-midnight font-bold text-xl mb-2">
+              <Link to={post.node.frontmatter.path}>
+                {post.node.frontmatter.title}
+              </Link>
+            </h3>
+            <div className="flex items-center justify-center sm:justify-start">
+              {
+                post.node.frontmatter.tags.split(', ').map(tag => (
+                  <span key={tag} className="inline-block bg-clouds rounded-full px-3 py-1 text-sm font-semibold text-concrete mx-1 sm:ml-0 sm:mr-2">#{tag}</span>
+                ))
+              }
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+    <div className="flex justify-end text-concrete">
+      <a href="#top">
+        <span className="align-middle">Go to top</span>
+        <FiArrowUp className="inline align-middle" />
+      </a>
+    </div>
   </Layout>
 );
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(
+      sort: {
+        fields: [frontmatter___date],
+        order: [DESC],
+      },
+      limit: 10
+    ) {
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            title
+            date
+            tags
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
